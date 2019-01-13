3a42d2e132278b7a8a456a985815e84de536369f
Alexa WANG <BEER@alexademacbook.local>
2019-01-13 19:47:57 +0800

    Initial Commit

---
+++ Final_1.0.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final_1.0.1
+//
+//  Created by Alexa Wang on 2019/1/13.
+//  Copyright © 2019 Alexa Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
